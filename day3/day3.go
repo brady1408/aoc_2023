8348000dab39a4c1ce9fde11932cfd9b2afd8c25
Brady Cole <brady.cole@udo.com>
2023-12-03 22:19:49 -0700

    day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -39,14 +39,13 @@ func isDigit(r rune) bool {
 	return digitCheck.MatchString(string(r))
 }
 
-func newSpecialGrid(input string) *specialGrid {
+func newSpecialGrid(gridInput string) *specialGrid {
 	var isNumberOrDot = regexp.MustCompile(`^[0-9\.]+$`)
 	g := &specialGrid{
 		grid: make(map[int]map[int]bool),
 	}
 
-	data := readInput()
-	for y, line := range strings.Split(data, "\n") {
+	for y, line := range strings.Split(gridInput, "\n") {
 		for x, char := range line {
 			if !isNumberOrDot.MatchString(string(char)) {
 				if g.grid[y] == nil {
@@ -76,12 +75,13 @@ func (s specialGrid) touchedBySpecial(x, y int) bool {
 
 func main() {
 	partOne()
+	partTwo()
 }
 
 func partOne() {
-	g := newSpecialGrid(input)
 	allTouched := []int{}
 	data := readInput()
+	g := newSpecialGrid(data)
 	for y, line := range strings.Split(data, "\n") {
 		number := ""
 		touched := false
@@ -110,6 +110,99 @@ func partOne() {
 	for _, n := range allTouched {
 		sum += n
 	}
-	fmt.Println(allTouched)
+	fmt.Println(sum)
+}
+
+type gearGrid struct {
+	grid map[int]map[int]bool
+}
+
+func (g gearGrid) getTouchedLocation(x, y int) []string {
+	startX := x - 1
+	startY := y - 1
+	touches := []string{}
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if g.grid[startY+i][startX+j] {
+				touches = append(touches, fmt.Sprintf("%d:%d", startX+j, startY+i))
+			}
+		}
+	}
+
+	return touches
+}
+
+func newGearGrid(input string) *gearGrid {
+	g := &gearGrid{
+		grid: make(map[int]map[int]bool),
+	}
+
+	// data := readInput()
+	data := input
+	for y, line := range strings.Split(data, "\n") {
+		for x, char := range line {
+			if char == '*' {
+				if g.grid[y] == nil {
+					g.grid[y] = make(map[int]bool)
+				}
+				g.grid[y][x] = true
+			}
+		}
+	}
+	return g
+}
+
+func partTwo() {
+	data := readInput()
+	allTouched := make(map[string]map[int]bool)
+	g := newGearGrid(data)
+	for y, line := range strings.Split(data, "\n") {
+		number := ""
+		touchedGears := make(map[string]bool)
+		for x, char := range line {
+			if isDigit(char) {
+				number += string(char)
+				if touches := g.getTouchedLocation(x, y); len(touches) > 0 {
+					for _, s := range touches {
+						touchedGears[s] = true
+					}
+				}
+			}
+			if x == len(line)-1 || !isDigit(char) {
+				if len(touchedGears) > 0 && number != "" {
+					n, err := strconv.Atoi(number)
+					if err != nil {
+						log.Panic(err)
+					}
+					for k := range touchedGears {
+						if allTouched[k] == nil {
+							allTouched[k] = make(map[int]bool)
+						}
+						allTouched[k][n] = true
+					}
+				}
+				touchedGears = make(map[string]bool)
+				number = ""
+			}
+		}
+	}
+	sum := 0
+	ratios := []int{}
+	for _, v := range allTouched {
+		if len(v) == 2 {
+			ratio := 0
+			for k := range v {
+				if ratio == 0 {
+					ratio = k
+				} else {
+					ratio *= k
+				}
+			}
+			ratios = append(ratios, ratio)
+		}
+	}
+	for _, n := range ratios {
+		sum += n
+	}
 	fmt.Println(sum)
 }
